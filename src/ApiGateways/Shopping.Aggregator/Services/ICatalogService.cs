c6e2dabda8f93a8d01a543f06c779b55fcdf8ae4
Ivan Menkachev <menkachev@gmail.com>
2023-01-05 17:08:33 +0200

    Developing ShoppingController for Consuming Internal Microservices in Shopping.Aggr

--- src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
@@ -7,7 +7,7 @@ namespace Shopping.Aggregator.Services
     public interface ICatalogService
     {
         public Task<IEnumerable<CatalogModel>> GetCatalog();
-        public Task<IEnumerable<CatalogModel>> GetCatalog(string id);
+        public Task<CatalogModel> GetCatalog(string id);
         public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
     }
 }
