8664a8b0199c012a9e533d5baf9740d197729fbc
Ivan Menkachev <menkachev@gmail.com>
2023-01-06 15:20:02 +0200

    Refactoring of AspnetBasics Shopping Web Application Microservices.

---
+++ src/WebApps/AspnetRunBasics/Services/ICatalogService.cs
@@ -0,0 +1,14 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public interface ICatalogService
+    {
+        public Task<IEnumerable<CatalogModel>> GetCatalog();
+        public Task<CatalogModel> GetCatalog(string id);
+        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        public Task<CatalogModel> CreateCatalog(CatalogModel model);
+    }
+}
